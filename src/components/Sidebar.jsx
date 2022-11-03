83f12c68b2926fe2083794c69a82307c50ef835b
Geronimo Viscontti <visconttig@gmail.com>
2022-11-03 14:37:56 -0300

    Sidebar component.

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Stack } from "@mui/material";
+
+import { categories } from "../utils/constants.js";
+
+
+const selectedCategory = "New";
+
+const Sidebar = () => (
+    <Stack direction="row" sx={{ 
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%"}, 
+        flexDirection: { md: "column"},
+    }}>
+
+        { categories.map((category) => (
+            <button className="category-btn" style={{
+                background: category.name === selectedCategory && "#fc1503", color: "white"
+            }}>
+                <span>{category.icon}</span>
+                <span>{category.name}</span>
+            </button>
+        ))}
+
+    </Stack>
+);
+
+
+export default Sidebar;  
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Navbar } from "./Navbar.jsx";
 export { default as SearchFeed } from "./SearchFeed.jsx";
 export { default as VideoDetail } from "./VideoDetail.jsx";
 export { default as SearchBar } from "./SearchBar.jsx";
+export { default as Sidebar } from "./Sidebar.jsx";
