a288c7b5ad6d023f24cdd774b1ba2f4bfff237f0
Geronimo Viscontti <visconttig@gmail.com>
2022-11-03 06:43:53 -0300

    Add placeholder components and navigation.

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ChannelDetail = () => {
+    return (
+        <div>Channel Detail</div>
+    );
+};
+
+
+export default ChannelDetail;
---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SearchFeed = () => {
+    return (
+        <div>Search Feed</div>
+    );
+};
+
+
+export default SearchFeed;
