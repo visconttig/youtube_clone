faacd7a64b93cf31baef46f2d80fba9188579d36
Geronimo Viscontti <visconttig@gmail.com>
2022-11-04 02:19:37 -0300

    Create and style video cards.

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+
+import { demoProfilePicture } from "../utils/constants.js";
+
+const ChannelCard = () => {
+    return (
+        <div>
+            Im a ChannelCard ;)
+        </div>
+    );
+};
+
+
+export default ChannelCard;
